44a7f7f42d408934e0f0e6cc2188cffb442334c5
federerj <joel.federer-gsponer@roche.com>
2020-12-04 10:30:29 +0100

    quickfix

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
+
+# install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
---
+++ symlink-common.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -sf $(pwd)/git/.gitaliases $HOME/.gitaliases
+ln -sf $(pwd)/git/..gitignore_global $HOME/.gitignore_global
+
+ln -sf $(pwd)/bin $HOME
